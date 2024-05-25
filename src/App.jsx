7d22c6519c01a32a176be45ff6690c03e8889d68
Shalon27 <shalonmhrzn10@gmail.com>
2024-05-25 23:27:07 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from "./components/Hello";
+function App() {
+
+  return (
+    <div className="App">
+      <Hello name="Rob" message="Hey there !" emoji="👋" />
+      <Hello name="Shalon" message="Hey!" emoji="👋"  />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Hello.jsx
@@ -0,0 +1,8 @@
+function Hello({name, message, emoji}) {
+    return (
+    <div>
+        <h1> {message} {emoji} {name}</h1>;
+    </div>
+    );
+}
+export default Hello;
